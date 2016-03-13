775c63db6ba99eab462d48aa1882de3fd12f56ef
xiaoqiangzhao <zq496547199@126.com>
2016-03-13 10:01:42 +0800

    Create Hanoi_Tower.py
    ### Refer to Wiki
    
    The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower,[1] and sometimes pluralized) is a mathematical game or puzzle. It consists of three rods, and a number of disks of different sizes which can slide onto any rod. The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical shape.
    
    The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
    
    Only one disk can be moved at a time.
    Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
    No disk may be placed on top of a smaller disk.
    With three disks, the puzzle can be solved in seven moves. The minimum number of moves required to solve a Tower of Hanoi puzzle is 2n - 1, where n is the number of disks.

---
+++ Hanoi_Tower.py
@@ -0,0 +1,15 @@
+#! /home/b51816/Python-3.5.1/python
+
+### HAN NUO Tower
+def move(n,a,b,c):
+   if n==1 :
+      print(a,"-->",c)
+   else:
+      move(n-1,a,c,b)
+      move(1,a,b,c)
+      move(n-1,b,a,c)
+
+A = 'A'
+B = 'B'
+C = 'C'
+move(4,A,B,C)
