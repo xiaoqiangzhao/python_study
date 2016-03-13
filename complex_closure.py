f77f26424d13aecead9ad7e31118df440f6013a5
xiaoqiangzhao <zq496547199@126.com>
2016-03-13 10:40:16 +0800

    Update complex_closure.py

--- complex_closure.py
+++ complex_closure.py
@@ -10,3 +10,10 @@ def d(x):
    return x+2
 add_and_square = compose_h(b,d)
 print (add_and_square(1))
+
+## lambda
+def lambda_try(f,g):
+   return lambda x,y: f(x)+g(y)
+
+my_lambda_try = lambda_try(b,d)
+print(my_lambda_try(2,3))
