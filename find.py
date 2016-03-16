9bdedf8fcbb5f22fc893a4e7a681742bbccd0266
xiaoqiangzhao <zq496547199@126.com>
2016-03-16 16:04:24 +0800

    Update find.py

--- find.py
+++ find.py
@@ -1,15 +1,28 @@
 #! /home/b51816/localpython/python3/bin/python3
 import sys
 import os
-key_word = sys.argv[0]
-def found_file(d,w):
+find_type = sys.argv[2]
+current_dir = sys.argv[1]
+key_word = sys.argv[3]
+print("Current Directory : %s\nFind Type : %s\nKey Word: %s\n" % (os.path.abspath(current_dir),find_type,key_word))
+def my_search(d,t,w):   ## type dir word
+   tp = 'dir'
    c_d =os.path.abspath(d)
    # print(c_d)
-   dirs = [x for x in os.listdir(d) if os.path.isdir(x)]
-   files = [x for x in os.listdir(d) if os.path.isfile(x)]
+   dirs = [x for x in os.listdir(d) if os.path.isdir(os.path.join(c_d,x))]  ## need abs path for each dir and file found
+   files = [x for x in os.listdir(d) if os.path.isfile(os.path.join(c_d,x))]
+   # print("found: ",d,c_d,files)
    for m_d in dirs:
-      found_file(os.path.join(d,m_d),w)
-   for m_f in files:
-      if w in m_f:
-         print(os.path.join(c_d,m_f))
-found_file('.','bal')   ## example usage
+      my_search(os.path.join(d,m_d),t,w)
+   if tp in t:
+      # print('looking for dir',t)
+      for m_d in dirs:
+         if w in m_d:
+            print(os.path.join(c_d,m_d))
+   else:
+      # print('looking for file in ',c_d,files)
+      for m_f in files:
+         if w in m_f:
+            print(os.path.join(c_d,m_f))
+my_search(current_dir,find_type,key_word)
+
